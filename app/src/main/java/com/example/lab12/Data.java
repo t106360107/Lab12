55c79de55f55d763f2e124da0dcc5b87bf47135d
nekoo <t106360107@ntut.edu.tw>
2019-12-27 01:13:52 +0800

    Lab12

---
+++ app/src/main/java/com/example/lab12/Data.java
@@ -0,0 +1,12 @@
+package com.example.lab12;
+
+public class Data {
+    Result result;
+    class Result{
+        Results[] results;
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
